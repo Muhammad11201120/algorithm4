17217a4c30bcf9390675028f999be05db62f682c
mo <muha.11201120@gmail.com>
2023-03-26 00:10:14 +0300

    adding new solutions and fumctions

--- DaysHoursMinutesInAMonth/DaysHoursMinutesInAMonth.cpp
+++ DaysHoursMinutesInAMonth/DaysHoursMinutesInAMonth.cpp
@@ -9,16 +9,44 @@ int readNumber() {
 bool isAleapYear(int year) {
 	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
-short numberOfDaysInMonth(short year) { return isAleapYear(year) ? 29 : 28; }
-short numberOfHoursInMonth(short year) { return numberOfDaysInMonth(year) * 24; }
-int numberOfMinutesInMonth(short year) { return numberOfHoursInMonth(year) * 60; }
-int numberOfSecondsInMonth(short year) { return numberOfMinutesInMonth(year) * 60; }
+short numberOfDaysInMonth(int year, short month) {
+	if (month < 1 || month>12)
+	{
+		return 0;
+	}
+	if (month == 2)
+	{
+		if (isAleapYear(year))
+		{
+			return 29;
+		}
+		return  28;
+	}
+	short arr[7] = { 1,3,5,7,8,10,12 };//months that have 31 days
+	for (short i = 0; i <= 7; i++)
+	{
+		if (arr[i - 1] == month)
+		{
+			return 31;
+		}
+	}
+	return 30; // if not month 2 or month with 31 days 
+}
+bool shortHandNumberOfDaysInMonth(int year, short month) {
+	if (month < 1 || month > 12)
+		return 0;
+	short arr[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return (month == 2) ? (isAleapYear(year) ? 29 : 28) : arr[month - 1];
+}
+short numberOfHoursInMonth(int year, short month) { return numberOfDaysInMonth(year, month) * 24; }
+int numberOfMinutesInMonth(int year, short month) { return numberOfHoursInMonth(year, month) * 60; }
+int numberOfSecondsInMonth(int year, short month) { return numberOfMinutesInMonth(year, month) * 60; }
 int main()
 {
 	int year = readNumber();
 	short month = readNumber();
-	cout << "Number Of Days " << month << " => " << numberOfDaysInMonth(year) << endl;
-	cout << "Number Of Hours " << month << " => " << numberOfHoursInMonth(year) << endl;
-	cout << "Number Of Minutes " << month << " => " << numberOfMinutesInMonth(year) << endl;
-	cout << "Number Of Seconds " << month << " => " << numberOfSecondsInMonth(year) << endl;
+	cout << "Number Of Days " << month << " => " << numberOfDaysInMonth(year, month) << endl;
+	cout << "Number Of Hours " << month << " => " << numberOfHoursInMonth(year, month) << endl;
+	cout << "Number Of Minutes " << month << " => " << numberOfMinutesInMonth(year, month) << endl;
+	cout << "Number Of Seconds " << month << " => " << numberOfSecondsInMonth(year, month) << endl;
 }
